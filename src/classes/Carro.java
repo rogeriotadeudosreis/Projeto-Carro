2d1e991380450caf99d1f6d244deddc660535c31
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr@hotmail.com>
2021-03-17 07:29:44 -0300

    Repository init

---
+++ src/classes/Carro.java
@@ -0,0 +1,26 @@
+package classes;
+
+public class Carro {
+
+    // atributos
+    private String placa = "";
+    private String modelo = "";
+
+    // métodos getters e setters
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+}
